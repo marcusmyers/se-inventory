47665238d126f500ba4ca28002c61ff1a8203743
Mark Myers <marcusmyers@gmail.com>
2014-12-10 08:07:14 -0500

    Updated to read from agent file and write to db file

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var http = require('http');
 var os   = require('os');
+var fs   = require('fs');
+var jf   = require('jsonfile');
+
+var file = './config/agent.json'
+var config = jf.readFileSync(file);
+
+var db = jf.readFileSync('./data/db.json');
+
+db.tag = config.tag;
+
+console.log(db);
+
+jf.writeFileSync('./data/db.json', db);
+
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
-
+  res.end(JSON.stringify(db));
+}).listen(config.port, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:'+config.port+'/');
+/*
 console.log('tmp dir: ', os.tmpdir());
 console.log('hostname: ', os.hostname());
 console.log('type: ', os.type());
@@ -18,3 +32,4 @@ console.log('load avg:', os.loadavg());
 console.log('total memory:', os.totalmem());
 console.log('free memory:', os.freemem());
 console.log('network interfaces:', os.networkInterfaces());
+*/
